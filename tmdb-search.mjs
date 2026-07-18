5f5b978e225467da2949f2f1e1a5842187dbf102
CJ-Christian-Jones <106883402+CJ-Christian-Jones@users.noreply.github.com>
2026-07-17 20:10:28 -0700

    Added tmdb-test.mjs

---
+++ tmdb-search.mjs
@@ -0,0 +1,14 @@
+// TMDB MOVIE SEARCH TEST
+//
+// This file searches TMDB using a movie title.
+//
+// HOW TO RUN:
+//
+// 1. Open the VS Code terminal.
+//
+// 2. Add your TMDB token to the current PowerShell session:
+//    $env:TMDB_ACCESS_TOKEN="PASTE_YOUR_FULL_TOKEN_HERE"
+//
+// 3. Run this file and place a movie title after the filename:
+//    node .\tmdb-search.mjs "Gravity"
+
